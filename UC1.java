1564788b01d93285a97866e7c1ba64e607e89a6f
surajlangute <surajlangute55@gmail.com>
2022-10-14 14:56:07 +0530

    Employee  Present or Absent

---
+++ UC1.java
@@ -0,0 +1,14 @@
+package day9_OOps_UC9;
+
+public class UC1 {
+	public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage computation program");
+        int randomCheck = (int)(Math.random()*2);
+        if(randomCheck == 0){
+            System.out.println("employee is Absent");
+        }
+        else if(randomCheck == 1){
+            System.out.println("employee is present");
+        }
+    }
+}
